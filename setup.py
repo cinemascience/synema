fedcdccc4aeb43c43ff34304622018706cd428bd
Li-Ta Lo <ollie@lanl.gov>
2024-03-06 16:23:27 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Synema',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='BSD',
+    author='Li-Ta Lo',
+    author_email='ollie@lanl.gov',
+    description='View Synthesis for Cinema'
+)
