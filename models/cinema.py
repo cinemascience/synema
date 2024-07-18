1a2436494f3324e3ea4da9fbc47446df59911657
Li-Ta Lo <ollie@lanl.gov>
2024-07-18 11:11:56 -0600

    semi-finalize design of the cinema model

--- models/cinema.py
+++ models/cinema.py
@@ -2,14 +2,22 @@ import flax.linen as nn
 import jax.numpy as jnp
 import jax.random
 
-from encoders.frequency import PositionalEncodingNeRF
+from encoders.hashgrid import HashGridEncoder
 from models.siren import Siren
 
 
 class CinemaScalarImage(nn.Module):
-    num_hidden_features = 128
-    # position_encoder: HashGridEncoder = HashGridEncoder(num_levels=8, feature_dims=4, table_size=2 ** 19)
-    position_encoder: PositionalEncodingNeRF = PositionalEncodingNeRF(num_frequencies=10)
+    num_hidden_features = 64
+    # When using SIREN, the hash_init_scale needs to be larger than described
+    # in the InstantNGP paper to prevent vanishing gradient.
+    # The goal is to make the encoded features to stretch across [-1, 1] as
+    # possible but not to "overflow" the [-1, 1] range when multiplied by the
+    # weights in the first layer in SIREN.
+    position_encoder: HashGridEncoder = HashGridEncoder(num_levels=8,
+                                                        table_size=2 ** 19,
+                                                        feature_dims=4,
+                                                        max_resolution=2 ** 12,
+                                                        hash_init_scale=1.e-1)
 
     def init_last(self, key, shape, dtype):
         v = jnp.sqrt(6. / shape[0]) / 30.0
@@ -18,31 +26,6 @@ class CinemaScalarImage(nn.Module):
 
     @nn.compact
     def __call__(self, input_points, input_views=None):
-        # Simple MLP with SIREN
-        # encoded_points = self.position_encoder(input_points)
-        # x = Sine(hidden_features=self.num_hidden_features, is_first=True)(encoded_points)
-        # for i in range(4):
-        #     x = Sine(hidden_features=self.num_hidden_features, is_first=False)(x)
-        #     if i == 3:
-        #         x = jnp.concatenate([x, encoded_points], axis=-1)
-        # x = nn.Dense(features=2, kernel_init=self.init_last)(x)
-        # density, scalar = jnp.split(x, [1], axis=-1)
-        # density = nn.relu(density)
-        # return scalar, jnp.squeeze(density)
-
-        # Simple MLP
-        # encoded_points = self.position_encoder(input_points)
-        # x = encoded_points
-        # for i in range(6):
-        #     x = nn.Dense(features=self.num_hidden_features)(x)
-        #     x = nn.relu(x)
-        #     if i == 4:
-        #         x = jnp.concatenate([x, encoded_points], axis=-1)
-        # x = nn.Dense(features=2)(x)
-        # density, scalar = jnp.split(x, [1], axis=-1)
-        # density = nn.relu(density)
-        # return scalar, jnp.squeeze(density)
-
         encoded_points = self.position_encoder(input_points)
         x = Siren(hidden_features=self.num_hidden_features, hidden_layers=4, out_features=16)(encoded_points)
         density, scalar = jnp.split(x, [1], axis=-1)
@@ -51,26 +34,3 @@ class CinemaScalarImage(nn.Module):
         scalar = jnp.concatenate([scalar, encoded_points], axis=-1)
         scalar = Siren(hidden_features=self.num_hidden_features, hidden_layers=2)(scalar)
         return scalar, jnp.squeeze(density)
-
-        # encoded_points = self.position_encoder(input_points)
-        # # density MLP, two layers of ReLU
-        # x = nn.Dense(features=self.num_hidden_features)(encoded_points)
-        # x = nn.relu(x)
-        # x = nn.Dense(features=self.num_hidden_features)(x)
-        # x = nn.relu(x)
-        # x = nn.Dense(features=self.num_hidden_features)(x)
-        # x = nn.relu(x)
-        # x = nn.Dense(features=16)(x)
-        #
-        # density, scalar = jnp.split(x, [1], axis=-1)
-        # density = nn.relu(density)
-        #
-        # # scalar MLP, two layers of ReLU
-        # scalar = jnp.concatenate([scalar, encoded_points], axis=-1)
-        # scalar = nn.Dense(features=self.num_hidden_features)(scalar)
-        # scalar = nn.relu(scalar)
-        # scalar = nn.Dense(features=self.num_hidden_features)(scalar)
-        # scalar = nn.relu(scalar)
-        # scalar = nn.Dense(features=1)(scalar)
-        #
-        # return scalar, jnp.squeeze(density)
